8e2f2262b34590a482f30e1335c8667d16d6ff03
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2019-04-15 12:05:12 +0900

    現状の引力に引き寄せられる＋その際の上下左右移動まで

--- SpaceStarSpeed/Assets/1_Nakashima/Script/PlayerMove.cs
+++ SpaceStarSpeed/Assets/1_Nakashima/Script/PlayerMove.cs
@@ -25,14 +25,17 @@ public class PlayerMove : MonoBehaviour
     }
     void FixedUpdate()
     {
-        //WASDで移動
+        //移動時に何かエフェクトを入れてどこに向かっているのかを分かるようにする？的な感じ
+
+        //上下左右前後の移動になる追加予定、上下運動に対してはON,OFFの切り替えがしやすいようなプログラムを書く
+        //前後左右移動
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(Vector3.forward * Speed,ForceMode.Force);           
+            rb.AddForce(Vector3.up * Speed,ForceMode.Force);           
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(Vector3.back * Speed, ForceMode.Force);
+            rb.AddForce(Vector3.down * Speed, ForceMode.Force);
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -42,27 +45,48 @@ public class PlayerMove : MonoBehaviour
         {
             rb.AddForce(Vector3.right * Speed, ForceMode.Force);
         }
+        //上下移動
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    rb.AddForce(Vector3.up * Speed, ForceMode.Force);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    rb.AddForce(Vector3.down * Speed, ForceMode.Force);
+        //}
     }
     void OnCollisionEnter(Collision col)
     {
         //現状Tagの設定をしていないので今後加えることになるんだと思う
-
-        //ここは一定以上の速度が出ているかの判定
-        if (rb.velocity.magnitude < Pmagnitude)
+        //↓のPlayerは初期からあったものを使っているだけ、まだTag名については考えていない
+        if (col.gameObject.tag == "Player")
         {
-            //速度の確認用
-            Debug.Log(rb.velocity.magnitude);
-            //速度を0にする（今後変える可能性が高い）
-            rb.velocity = Vector3.zero;
-            //衝突したオブジェクトを消す、漂ってる星が消えるはず
-            Destroy(col.gameObject);      
+            //ここは一定以上の速度が出ているかの判定
+            if (rb.velocity.magnitude < Pmagnitude)
+            {
+                //速度の確認用
+                Debug.Log(rb.velocity.magnitude);
+                //速度を0にする（今後変える可能性が高い）
+                rb.velocity = Vector3.zero;
+                //衝突したオブジェクトを消す、漂ってる星が消えるはず
+                Destroy(col.gameObject);
+            }
+            else
+            {
+                //自キャラを消す
+                Destroy(gameObject);
+                //速度の確認用
+                Debug.Log(rb.velocity.magnitude);
+            }
         }
-        else
+        
+        //これはブラックホール？に当たった際の処理、これもTag名については何も考えていない
+        if(col.gameObject.tag == "Finish")
         {
-            //自キャラを消す
+            //消す
             Destroy(gameObject);
-            //速度の確認用
-            Debug.Log(rb.velocity.magnitude);
-        }      
+            //ここで何かアクションを出すことになるはず
+
+        }
     }
 }

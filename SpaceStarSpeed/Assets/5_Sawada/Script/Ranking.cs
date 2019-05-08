0809a30c6cd6ce60d6918aeb042ac007dcec5a27
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-05-08 14:59:28 +0900

    タイムランキング

--- SpaceStarSpeed/Assets/5_Sawada/Script/Ranking.cs
+++ SpaceStarSpeed/Assets/5_Sawada/Script/Ranking.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Ranking : MonoBehaviour
 {
-    
+    //0
+    int startpoint = 0;
+    //分
     int[] minute = new int[5];
+    //秒
     int[] second = new int[5];
-    int point=Scene2.scoa;
+    //クリア時間
+    int point=Timar.timepoint;
+    //ランキングを5位まで表示
     string[] ranking = { "ランキング1位", "ランキング2位", "ランキング3位", "ランキング4位", "ランキング5位" };
     int[] rankingValue = new int[5];
     [SerializeField, Header("表示させるテキスト")]
@@ -16,16 +21,29 @@ public class Ranking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //クリア時間が0ならば1時間にする
+        if (point == 0)
+        {
+            point = 3600;
+        }
         //PlayerPrefs.DeleteAll();
+        //ランキング呼び出し
         GetRanking();
+        //ランキングを書き込む
         SetRanking(point);
         for (int i = 0; i < rankingText.Length; i++)
         {
+         
+           //ランキング表示
             minute[i] = (int)rankingValue[i] / 60;
             second[i] = rankingValue[i] - minute[i] * 60;
+            rankingText[i].text = (i + 1) + "位" + minute[i].ToString("00") + ";" + ((int)second[i]).ToString("00");
+            //クリア時間が0や1時間ならば表示しない
+            if (rankingValue[i] == 0 || rankingValue[i] == 3600)
+            {
+                rankingText[i].text = (i+1)+"位"+"-------";
 
-            rankingText[i].text =(i+1)+"位" + minute[i].ToString("00")+";"+ ((int)second[i]).ToString("00");
-
+            }
         }
     
 }
@@ -34,22 +52,43 @@ public class Ranking : MonoBehaviour
         //ランキング呼び出し
         for (int i = 0; i < ranking.Length; i++)
         {
+
+          
             rankingValue[i] = PlayerPrefs.GetInt(ranking[i]);
+
         }
     }
     void SetRanking(int _value)
     {
+        
         //書き込み用
         for (int i = 0; i < ranking.Length; i++)
         {
-            if (_value > rankingValue[i])
+            //ランキングに0が表示されていたら0を無視して新しい記録を入れる
+            if (_value * rankingValue[i] == startpoint)
             {
-
-                var change = rankingValue[i];
+                var chand =rankingValue[i];
                 rankingValue[i] = _value;
-                _value = change;
+                _value = chand;
+              
             }
+            //それ以外の場合はランキングに表示されている数よりクリア時間が短い場合はランキングを更新する
+            else
+            {
+                if (_value < rankingValue[i])
+                {
+
+
+
+                    var change = rankingValue[i];
+                    rankingValue[i] = _value;
+                    _value = change;
+
                 }
+            }
+            
+        }
+                
             
         
 
@@ -57,7 +96,7 @@ public class Ranking : MonoBehaviour
         for (int i = 0; i < ranking.Length; i++)
         {
           
-            PlayerPrefs.SetInt(ranking[i], rankingValue[i]);
+            PlayerPrefs.SetInt(ranking[i],rankingValue[i]);
            
 
         }
--- SpaceStarSpeed/Assets/5_Sawada/Script/Timar.cs
+++ SpaceStarSpeed/Assets/5_Sawada/Script/Timar.cs
@@ -3,26 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 public class Timar : MonoBehaviour
 {
+    //時間を4桁まで表示する
     [SerializeField]
     Image[] images = new Image[4];
 
-
+    //0～9までの数字を表示する
     [SerializeField]
     Sprite[] numberSprites = new Sprite[10];
-
-    public float timeCount { get; private set; }
+    //クリアまでの時間を計測する
+    public   float timeCount { get; private set; }
+    public static int timepoint;
     // Start is called before the first frame update
     void Start()
     {
+        //タイマースタート
         SetTime(0);
     }
+    //クリア時間を計測する
     public void SetTime(float time)
     {
         timeCount = time;
         StartCoroutine(TimerStart());
     }
+    //タイマーに使う画像を表示する
     void SetNumbers(int sec, int val1, int val2)
     {
         string str = String.Format("{0:00}", sec);
@@ -37,16 +43,24 @@ public class Timar : MonoBehaviour
             SetNumbers(sec, 2, 3);
             int minu = Mathf.FloorToInt((timeCount -sec) / 60);
             SetNumbers(minu, 0, 1);
+         
             yield return new WaitForSeconds(1.0f);
             timeCount += 1.0f;
+            timepoint = (int)timeCount;
+            Debug.Log(timeCount);
+           
         }
-     
+    
     }
 
    
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SceneManager.LoadScene("New Scene");
+
+        }
     }
 }

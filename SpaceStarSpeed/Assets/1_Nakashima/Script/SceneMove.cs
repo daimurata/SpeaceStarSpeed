edade77f17ccf3b786413ad17164a1fa6ce406de
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2019-05-10 13:32:46 +0900

    ステージ選択のUI的なものの作成途中

---
+++ SpaceStarSpeed/Assets/1_Nakashima/Script/SceneMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    //シーンにただ飛ばすだけ
+    //シーンの名前を入力
+    public string SceneName;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ButtonClick()
+    {
+        //シーンの名前を表示してそのシーンに移動
+        Debug.Log(SceneName);
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ SpaceStarSpeed/Assets/1_Nakashima/Script/SceneSelectScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    //シーンの選択中に必要な処理を追加していく
+    //子
+    public GameObject[] childStageObj = new GameObject[4]; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //ここのとっちゅう
+
+        int j = 0;
+        //選択中のGameObjectを取得
+        GameObject go = EventSystem.current.currentSelectedGameObject;
+        foreach(Transform child in go.transform)
+        {
+            //childStageObj[j] = child.gameObject;
+            //Stage1のobjをtrueにする
+            if(j == 0)
+            {
+                childStageObj[j].SetActive(true);
+                j++;
+            }
+            //Stage2のobjをtrueにする
+            if (j == 1)
+            {
+                childStageObj[j].SetActive(true);
+                j++;
+            }
+            //Stage3のobjをtrueにする
+            if (j == 2)
+            {
+                childStageObj[j].SetActive(true);
+                j++;
+            }
+            //Stage4のobjをtrueにする
+            if (j == 3)
+            {
+                childStageObj[j].SetActive(true);
+                j++;
+            }
+        }
+        Debug.Log(go);
+    }
+}

764a55e8be2c0baf7dd0f87cf9a6ea046db29a3d
daimurata <nkc.dai.murata@gmail.com>
2019-06-06 14:47:50 +0900

    ゲームオーバーを表示設定できるようにした。 ステージ１に爆発オブジェk追加

--- SpaceStarSpeed/Assets/1_Nakashima/Script/Gravity.cs
+++ SpaceStarSpeed/Assets/1_Nakashima/Script/Gravity.cs
@@ -15,6 +15,8 @@ public class Gravity : MonoBehaviour
     public float a;
 
     bool isActive = false;
+
+    public GameObject EX;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +43,7 @@ public class Gravity : MonoBehaviour
     {
         if(col.gameObject.tag == "Finish")
         {
+            EX.SetActive(true);
             Destroy(gameObject);
         }   
     }
@@ -51,6 +54,7 @@ public class Gravity : MonoBehaviour
         //Tag名きまってませーん、仮でPlayer
         if(other.gameObject.tag == "Player")
         {
+            EX.SetActive(true);
             isActive = true;
         }
     }
